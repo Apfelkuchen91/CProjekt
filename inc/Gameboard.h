0460efbdbec373aeb8d480381394888b82520344
Apfelkuchen91 <Apfelkuchen91>
2013-06-14 13:45:37 +0200

    [Gamebord] Klasse erstellt
    Ohne Inhalt, wird noch implementiert.

---
+++ inc/Gameboard.h
@@ -0,0 +1,19 @@
+/*!
+ * @file	Gameboard.h
+ *
+ * @date	14.06.2013
+ * @author	Superuser
+ * @brief	Gameboard.h
+ */
+
+#ifndef GAMEBOARD_H_
+#define GAMEBOARD_H_
+
+class Gameboard
+{
+	public:
+		Gameboard();
+		virtual ~Gameboard();
+};
+
+#endif /* GAMEBOARD_H_ */
---
+++ src/Gameboard.cpp
@@ -0,0 +1,21 @@
+/*!
+ * @file	Gameboard.cpp
+ *
+ * @date	14.06.2013
+ * @author	Superuser
+ * @brief	Gameboard.cpp
+ */
+
+#include "Gameboard.h"
+
+Gameboard::Gameboard()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+Gameboard::~Gameboard()
+{
+	// TODO Auto-generated destructor stub
+}
+
